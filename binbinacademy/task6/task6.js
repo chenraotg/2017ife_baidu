565fe82603065582e5f463e6f25ae8340464645d
tianqingfish <leochen19931029@gmail.com>
2017-03-26 12:13:28 +0800

    task6

--- binbinacademy/task6/task6.js
+++ binbinacademy/task6/task6.js
@@ -1,20 +1,28 @@
 var btnSearch = document.getElementById("search");
-var btnInput = document.getElementById("input");
+var btnInput = document.getElementById("generate");
 
 btnInput.onclick = function(){
 	var textArea = document.getElementById("textArea");	
 	var txt = textArea.value;	//多行文本域的值
-	var input = document.getElementById("input");	//	
-	var searchText = input.value;	//输入框的值
-	var pattern = new RegExp("[\,\.\、\\n\\s]","g");
-	var arr = txt.split(pattern,txt);
+	//console.log(txt);
+	var place = document.getElementById("place");	//获得place
+	var pattern = new RegExp("[\,\;\n\\s\、\.\。\，]","g");
+	var arr = txt.split(pattern);
+	console.log(arr);
 	for(var i = 0;i < arr.length;i++){
-		
-		
+		var node = document.createElement("span");
+		node.innerText = arr[i];
+		place.appendChild(node);
 	}
 }
 
 btnSearch.onclick = function (){
-	
-	
+	var span = document.getElementsByTagName("span");
+	var searchText = document.getElementById("input").value;
+	for (var i = 0;i < span.length;i++){
+		console.log(searchText == span[i].innerText);
+		if (searchText == span[i].innerText){
+			span[i].className = "finished";
+		}
+	}
 }
