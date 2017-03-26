1a65781adfe39fbd1ddad774ec51dcdd001338b1
tianqing <leochen19931029@gmail.com>
2017-03-26 14:22:04 +0800

    task6 has been finished!

--- binbinacademy/task6/task6.js
+++ binbinacademy/task6/task6.js
@@ -4,10 +4,10 @@ var btnInput = document.getElementById("generate");
 btnInput.onclick = function(){
 	var textArea = document.getElementById("textArea");	
 	var txt = textArea.value;	//多行文本域的值
-	//console.log(txt);
 	var place = document.getElementById("place");	//获得place
+	place.innerHTML = "";
 	var pattern = new RegExp("[\,\;\n\\s\、\.\。\，]","g");
-	var arr = txt.split(pattern);
+	arr = txt.split(pattern);
 	console.log(arr);
 	for(var i = 0;i < arr.length;i++){
 		var node = document.createElement("span");
@@ -18,10 +18,13 @@ btnInput.onclick = function(){
 
 btnSearch.onclick = function (){
 	var span = document.getElementsByTagName("span");
+	for (var i = 0;i < span.length;i++){
+		span[i].className = "";
+	}
 	var searchText = document.getElementById("input").value;
 	for (var i = 0;i < span.length;i++){
-		console.log(searchText == span[i].innerText);
-		if (searchText == span[i].innerText){
+		console.log(typeof(arr[i]));
+		if (arr[i].indexOf(searchText) != -1){
 			span[i].className = "finished";
 		}
 	}
