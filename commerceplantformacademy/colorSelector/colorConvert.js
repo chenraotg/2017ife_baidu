d9c9cb4f42e6265261e76292beb5f16853ec2818
tianqing <leochen19931029@gmail.com>
2017-06-15 21:11:11 +0800

    colorSelector

---
+++ commerceplantformacademy/colorSelector/colorConvert.js
@@ -0,0 +1,130 @@
+let hsv2rgb = function(h, s, v){
+	let r, g, b;
+	
+	let i = Math.floor(h / 60);
+	let f = h / 60 - i;
+	let p = v * (1 - s);
+	let q = v * (1 - f * s);
+	let t = v * (1 - (1 - f) * s);
+	
+	switch (i % 6){
+		case 0:
+			r = v, g = t, b = p;
+			break;
+		case 1:
+			r = q, g = v, b = p;
+			break;
+		case 2:
+			r = p, g = v, b = t;
+			break;
+		case 3:
+			r = p, g = q, b = v;
+			break;
+		case 4:
+			r = t, g = p, b = v;
+			break;
+		case 5:
+			r = v, g = p, b = q;
+			break;
+	}
+	
+	return [fixed(r * 255), fixed(g * 255), fixed(b * 255)];
+}
+
+let rgb2hsv = function(r, g, b){
+	r /= 255, g /= 255, b /= 255;
+	
+	let max = Math.max(r, g, b),
+		min = Math.min(r, g, b);
+		
+	let h, s, v;
+	v = max;
+	
+	let d = max - min;
+	s = max == 0 ? 0 : d / max;
+	
+	if (max == min){
+		h = 0;
+	} else {
+		switch (max){
+			case r:
+				h = 60 * ((g - b) / d + (g < b ? 6 : 0));
+				break;
+			case g:
+				h = 60 * ((b - r) / d + 2);
+				break;
+			case b:
+				h = 60 * ((r - g) / d + 4);
+				break;
+		}
+	}
+	
+	return [fixed(h), fixed(s, 2), fixed(v, 2)];
+}
+
+let hsl2rgb = function(h, s, l) {
+    h /= 360;
+
+    let r, g, b;
+
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        }
+
+        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        let p = 2 * l - q;
+
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [fixed(r * 255), fixed(g * 255), fixed(b * 255)];
+}
+
+let rgb2hsl = function(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+
+    let max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        let d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        switch (max) {
+            case r:
+                h = 60 * ((g - b) / d + (g < b ? 6 : 0));
+                break;
+            case g:
+                h = 60 * ((b - r) / d + 2);
+                break;
+            case b:
+                h = 60 * ((r - g) / d + 4);
+                break;
+        }
+    }
+
+    return [fixed(h), fixed(s, 2), fixed(l, 2)];
+}
+
+let hsv2hsl = function(h, s, v) {
+    let rgbArr = hsv2rgb(h, s, v);
+    return rgb2hsl(rgbArr[0], rgbArr[1], rgbArr[2]);
+}
+
+let hsl2hsv = function(h, s, l) {
+    let rgbArr = hsl2rgb(h, s, l);
+    return rgb2hsv(rgbArr[0], rgbArr[1], rgbArr[2]);
+}
--- commerceplantformacademy/colorSelector/colorSelect.js
+++ commerceplantformacademy/colorSelector/colorSelect.js
@@ -1,3 +1,179 @@
+
+//条形选择器构造器，传入的分别是颜色的初始值（HSV）数组，初始位置，最大位置，颜色条和按钮的DOM
+let BarSelector = function(defaultColor, defaultPos, maxpos, bar, barBall){
+	this.color = defaultColor;
+	this.barBallPos = defaultPos;
+	this.maxPos = maxpos;
+	this.bar = bar;
+	this.bar.self = this;
+	this.barBall = barBall;
+	this.barBall.self = this;
+}
+
+BarSelector.prototype.pick = function(){
+	let h = fixed((this.barBallPos / this.maxPos) * 360);
+	let s = 1;
+	let v = 1;
+	
+	this.color = [h, s, v];
+	
+	let rgbArr = hsv2rgb(h, s, v);//获得rgb颜色
+	
+	let color = 'rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})';
+	this.barBall.style.background = color;//设置按钮颜色
+	
+	colorPanel.render(color);//渲染取色板
+}
+
+BarSelector.prototype.setPos = function(e){
+	this.barBallPos = e.offsetY;
+	this.btn.style.top = this.barBallPos + 'px';
+	
+	this.pick();
+	
+	
+	
+}
+
+BarSelector.prototype.move= function(e){
+	let oldPos = this.oldPos;
+	
+	let startPos = this.startPos;
+	
+	let distance = e.pageY - startPos;
+	
+	this.barBallPos = oldPos + distance;
+	
+	if (this.barBallPos < 0){
+		this.barBallPos = 0;
+	} else if (this.barBallPos > this.maxPos){
+		this.barBallPos = this.maxPos;
+	}
+	
+	this.barBall.style.top = this.barBallPos + 'px';
+	
+	this.pick();
+}
+
+//条形选择器实例
+let barSelector = (function(){
+	let defaultColor = [360, 1, 1];//hsv
+	
+	let bar = document.getElementById("bar");
+	let barBall = document.getElementById("barBall");
+
+	bar.onclick = function(e){
+		
+	}
+	
+	barBall.addEventListener('mousedown', function(e){
+		e.preventDefault();
+		
+		barBall.self.startPos = e.pageY;
+		barBall.self.oldPos = btn.self.btnPos;
+		window.addEventListener('mousemove', t);
+		
+	});
+	return new BarSelector(defaultColor, 0, 400, bar, barBall);
+})();
+
+//面板选择器构造器，传入的是按钮实例
+let PanelSelector = function(defaultX, defaultY, maxX, maxY, btn, canvas, pickedColorE1, value){
+	this.color = [];
+	this.x = defaultX;
+	this.y = defaultY;
+	this.maxX = maxX;
+	this.maxY = maxY;
+	this.btn = btn;
+	this.btn.self = this;
+	this.panel = panel;
+	this.panel.self = this;
+	this.pickedColorE1 = pickedColorE1;
+	this.value = value;
+	
+}
+
+PanelSelector.prototype.pickByRGB = function(){//根据RGB值进行取色
+	let r = selectors.rgb.R.val;
+	let g = selectors.rgb.G.val;
+	let b = selectors.rgb.B.val;
+	
+	let hsvArr = rgb2hsv(r, g, b);
+	
+	let h = hsvArr[0];
+	let s = hsvArr[1];
+	let v = hsvArr[2];
+	
+	this.x = s * this.maxX;
+	this.y = this.maxY - v * this.maxY;
+	this.btn.style.left = this.x + 'px';
+	this.btn.style.top = this.y + 'px';
+	
+	barSelector.btnPos = fixed(barSelector.maxPos * (h / 360));
+}
+
+PanelSelector.prototype.pickByHSL = function(){//根据HSL值进行取色
+	let h = selectors.hsl.H.val;
+	let s = selectors.hsl.S.val;
+	let l = selectors.hsl.L.val;
+	
+	let hsvArr = hsl2hsv(h, s, l);
+	
+	h = hsvArr[0];
+	s = hsvArr[1];
+	v = hsvArr[2];
+	
+	this.x = s * this.maxX;
+	
+}
+
+let panelSelector = (function(){
+	let panelBall = document.getElementById('panelBall');
+	
+	let panel = document.getElementById('panel');
+	
+	let pickedColorEl = document.getElementById('pickedColor');
+	
+	let rgb = document.getElementById('rgb');
+	let hsl = document.getElementById('hsl');
+	let hsv = document.getElementById('hsv');
+	
+	let value = {
+		rgb,
+		hsl,
+		hsv
+	};
+	
+	//事件绑定
+	panel.onclick = function(e){
+		panel.self.setPos(e);
+	}
+	
+	//这是啥？？
+	let t = throttleV2(function(e){
+		btn.self.move(e);
+	}, 15, 30);//必须确保调用点的this指向是PanelSelector对象
+	
+	panelBall.addEventListener('mousedown', function(e){
+		e.preventDefault();
+		btn.self.startPos = {
+			x:e.pageX,
+			y:e.pageY
+		}
+		panelBall.self.oldPos = {
+			x:panelBall.self.x,
+			y:panelBall.self.y
+		}
+		window.addEventListener('mousemove', t);
+	});
+	window.addEventListener('mouseup', function(){
+		window.removeEventListener('mousemove', t);
+	});
+	
+	return new PanelSelector(100, 0, 400, 400, panelBall, pickedColorEl, value);
+})();
+
+//鼠标点击后获得的颜色都是以hsv来计算，然后转换成rgb和hsl
 let colorPanel = (function(){
 	let canvas = document.getElementById('panel');
 	let ctx = canvas.getContext('2d');
@@ -25,25 +201,11 @@ let colorPanel = (function(){
 	}
 })();
 
-let colorBar = function(){
-	let 
+let init = (() => {
+	new Clipboard('#rgbBtn');
+	new Clipboard('#hslBtn');
+	new Clipboard('#hsvBtn');
 	
-}
-//条形选择器实例
-let barSelector = (function(){
-	let defaultColor = [360, 1, 1];//hsv
-	
-	let bar = document.getElementById("bar");
-	let barBall = document.getElementById("barBall");
-
-	bar.onclick = function(e){
-		
-	}
-	
-	barBall.addEventListener('mousedown', function(e){
-		e.preventDefault();
-		
-		btn.
-		
-	})
-})
+	barSelector.pick();
+	panelSelector.pick();
+})();
